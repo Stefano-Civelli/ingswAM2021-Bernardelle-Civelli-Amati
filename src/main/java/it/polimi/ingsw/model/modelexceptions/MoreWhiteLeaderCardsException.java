a4a27464a6b70d5c51127bfa6c160117116de290
Axelamati <alex.amati@mail.polimi.it>
2021-04-10 15:05:45 +0200

    Added MoreWhiteLeaderCardsException

---
+++ src/main/java/it/polimi/ingsw/model/modelexceptions/MoreWhiteLeaderCardsException.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model.modelexceptions;
+
+public class MoreWhiteLeaderCardsException extends Exception {
+
+    public MoreWhiteLeaderCardsException() {
+        super();
+    }
+
+    public MoreWhiteLeaderCardsException(String message) {
+        super(message);
+    }
+
+}
