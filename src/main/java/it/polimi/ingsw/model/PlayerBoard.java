3b0126f8ca922ef34c681ae9e3e11a55c7e24388
Pietro Bernardelle <pietro.bernardelle@mail.polimi.it>
2021-04-16 13:37:48 +0100

    added javadoc to PlayerBoard class.

--- src/main/java/it/polimi/ingsw/model/PlayerBoard.java
+++ src/main/java/it/polimi/ingsw/model/PlayerBoard.java
@@ -63,7 +63,7 @@ public class PlayerBoard implements InterfacePlayerBoard, MoveForwardObserver, M
 
    /**
     * remove 1 of the leader cards from leaderCards array in the beginning of the game
-    * @param leaderPosition, index of the leaderCard to remove
+    * @param leaderPosition, index of the leaderCard to remove (starts at 0)
     */
    public void discardLeaderAtBegin(int leaderPosition) throws InvalidLeaderCardException {
       if(leaderCards.size() < 3)
@@ -71,6 +71,12 @@ public class PlayerBoard implements InterfacePlayerBoard, MoveForwardObserver, M
       leaderCards.remove(leaderPosition);
    }
 
+   /**
+    * remove 1 of the leader cards from leaderCards array during the game
+    * once the card is removed the faith marker is moved forward by 1
+    * @param leaderPosition, index of the leaderCard to remove (starts at 0)
+    * @throws InvalidLeaderCardException if the leaderCard is activated or the index is outOfBound
+    */
    public void discardLeader(int leaderPosition) throws InvalidLeaderCardException {
       if(leaderCards.size() > 2 || leaderCards.get(leaderPosition).isActive())
          throw new InvalidLeaderCardException("U can't remove this card in this moment");
@@ -78,7 +84,14 @@ public class PlayerBoard implements InterfacePlayerBoard, MoveForwardObserver, M
       track.moveForward(1);
    }
 
-
+   /**
+    * pick a card from the DevelopCardDeck and place it in the CardSlot
+    * @param row, row of the Deck that identifies the Develop to add (starts at 0)
+    * @param column, column of the Deck that identifies the Develop to add (starts at 0)
+    * @param cardSlot, slot of CardSlots where the card is placed (starts at 0)
+    * @throws InvalidCardPlacementException if the card can't be placed in the slot passed as parameter
+    * @throws RowOrColumnNotExistsException if the index of row or column doesn't exists
+    */
    public void addDevelopCard(int row, int column, int cardSlot) throws InvalidCardPlacementException, RowOrColumnNotExistsException {
       cardSlots.addDevelopCard(cardSlot, developCardDeck.getCard(row, column));
    }
@@ -100,13 +113,12 @@ public class PlayerBoard implements InterfacePlayerBoard, MoveForwardObserver, M
       return new ArrayList<>(tempMarketMarble);
    }
 
-   public List<MarketMarble> setLeaderCardActive(int leaderToActivate) throws NotEnoughResourcesException, InvalidLeaderCardException {
-      if(!leaderCards.get(leaderToActivate).isActive())
-         leaderCards.get(leaderToActivate).setActive(this);
-      return new ArrayList<>(tempMarketMarble);
-   }
-
-
+   /**
+    * add one of the tempMarketMarble resources to warehouse
+    * @param marbleIndex, the index of the marble to remove from tempMarketMarble (starts at 0)
+    * @throws MoreWhiteLeaderCardsException if the player has more than 1 leaderCard activated that modifies the white marble
+    * @throws NotEnoughSpaceException if the resource can't be added
+    */
    public void addMarbleToWarehouse(int marbleIndex) throws MoreWhiteLeaderCardsException, NotEnoughSpaceException {
          if (marbleIndex < 0 || marbleIndex >= tempMarketMarble.size())
             throw new IndexOutOfBoundsException("The index of the marble u gave me doesn't match the length of my array");
@@ -123,6 +135,12 @@ public class PlayerBoard implements InterfacePlayerBoard, MoveForwardObserver, M
       tempMarketMarble.remove(marbleIndex);
    }
 
+   /**
+    * add a white marble to warehouse using the leaderCard passed as parameter
+    * @param leaderPosition index of the leader to use to modify the color of the white marble
+    * @throws InvalidLeaderCardException if the index is outOfBound
+    * @throws NotEnoughSpaceException if, after have modified the white marble into a resource, there isn't place in the warehouse to place it
+    */
    public void addWhiteToWarehouse(int leaderPosition) throws InvalidLeaderCardException, NotEnoughSpaceException {
       if (leaderPosition < 0 || leaderPosition >= leaderCards.size())
          throw new InvalidLeaderCardException("The index of the leaderCard u gave me doesn't match the length of my array");
@@ -137,6 +155,13 @@ public class PlayerBoard implements InterfacePlayerBoard, MoveForwardObserver, M
       }
    }
 
+   /**
+    * allows the player to throw 2 resources in exchange for 1 resource he wants
+    * @param resource1, first resource the player wants to throw
+    * @param resource2, second resource the player wants to throw
+    * @param product, resource that the player wants to gain
+    * @throws AbuseOfFaithException if one of the 3 resources is faith, he can't throw faith and he can't gain faith
+    */
    public void baseProduction(ResourceType resource1, ResourceType resource2, ResourceType product) throws AbuseOfFaithException {
       if(warehouse.getNumberOf(resource1) + chest.getNumberOf(resource1) > 0 && warehouse.getNumberOf(resource2) + chest.getNumberOf(resource2) > 0) {
          tempResources = Stream.of(new Object[][]{
