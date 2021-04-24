a03a985d5580f67a3c413e5114b771ad1d61c8fd
Axelamati <alex.amati@mail.polimi.it>
2021-04-24 16:25:05 +0200

    Created EndTurnAction

---
+++ src/main/java/it/polimi/ingsw/network/Action/EndTurnAction.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.network.Action;
+
+import it.polimi.ingsw.controller.IGameState;
+import it.polimi.ingsw.controller.PhaseType;
+import it.polimi.ingsw.model.modelexceptions.ModelException;
+
+public class EndTurnAction extends Action {
+
+    @Override
+    public PhaseType performAction(IGameState gameState) throws InvalidActionException, ModelException {
+        if(!super.checkValid(gameState))
+            throw new InvalidActionException();
+        gameState.getGame().getPlayerBoard(super.userName).enterFinalTurnPhase();
+        return gameState.getCurrentPhase() == PhaseType.SETUP_DISCARDLEADER ? PhaseType.END_SETUP : PhaseType.END;
+    }
+
+}
