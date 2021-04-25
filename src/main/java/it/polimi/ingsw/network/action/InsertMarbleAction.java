0cc5953ece7c00795a3b151122b127d18e2b087a
Axelamati <alex.amati@mail.polimi.it>
2021-04-25 22:46:31 +0200

    Fixed InsertMarbleAction.performAction()

--- src/main/java/it/polimi/ingsw/network/action/InsertMarbleAction.java
+++ src/main/java/it/polimi/ingsw/network/action/InsertMarbleAction.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.network.action;
 import it.polimi.ingsw.controller.IGameState;
 import it.polimi.ingsw.controller.PhaseType;
 import it.polimi.ingsw.model.modelexceptions.ModelException;
+import it.polimi.ingsw.model.modelexceptions.MoreWhiteLeaderCardsException;
 
 public class InsertMarbleAction extends Action {
 
@@ -13,7 +14,11 @@ public class InsertMarbleAction extends Action {
     public PhaseType performAction(IGameState gameState) throws InvalidActionException, ModelException {
         if(!super.checkValid(gameState))
             throw new InvalidActionException();
-        gameState.getGame().getPlayerBoard(super.username).addMarbleToWarehouse(this.marbleIndex);
+        try {
+            gameState.getGame().getPlayerBoard(super.username).addMarbleToWarehouse(this.marbleIndex);
+        } catch (MoreWhiteLeaderCardsException e) {
+            return PhaseType.SHOPPING_LEADER;
+        }
         return gameState.getGame().getPlayerBoard(super.username).areMarblesFinished()
                 ? PhaseType.FINAL : PhaseType.SHOPPING;
     }
