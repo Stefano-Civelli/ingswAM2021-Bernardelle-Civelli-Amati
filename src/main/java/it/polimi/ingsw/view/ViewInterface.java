310acad449a370301c0e92c49110fe247f073bc0
Pietro Bernardelle <pietro.bernardelle@mail.polimi.it>
2021-04-25 22:53:10 +0100

    created Client, ServerConnector, Cli and ViewInterface classes.

--- src/main/java/it/polimi/ingsw/view/ViewInterface.java
+++ src/main/java/it/polimi/ingsw/view/ViewInterface.java
@@ -1,4 +1,18 @@
 package it.polimi.ingsw.view;
 
+
+import it.polimi.ingsw.network.messages.Message;
+
 public interface ViewInterface {
+  public void displaySetup();
+  public void displayLogin();
+  public void displaySetupFailure();
+  public void displayDisconnected();
+  public void displayFailedLogin(Message msg);
+  public void displayLoginSuccessful();
+  public void displayLobbyCreated();
+  public void displayOtherUserJoined(Message msg);
+  public void displayYouJoined();
+  public void displayPlayersNumberChoice();
+  public void displayWaiting();
 }
