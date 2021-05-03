8db1be99d6a4b546c7b6ad543c671802bc5210ab
Axelamati <alex.amati@mail.polimi.it>
2021-05-04 00:57:32 +0200

    Moved lorenzoTrackConfigFile from SinglePlayer to ConfigParameters. Made ConfigParameters final and not instantiable creating a private constructor.

--- src/main/java/it/polimi/ingsw/model/singleplayer/SinglePlayer.java
+++ src/main/java/it/polimi/ingsw/model/singleplayer/SinglePlayer.java
@@ -5,20 +5,19 @@ import it.polimi.ingsw.model.DevelopCardColor;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.modelexceptions.InvalidUsernameException;
 import it.polimi.ingsw.model.track.LorenzoTrack;
+import it.polimi.ingsw.utility.ConfigParameters;
 import it.polimi.ingsw.utility.GSON;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class SinglePlayer extends Game {
    private final LinkedList<ActionToken> actionTokenStack; // don't change with List, it's required a LinkedList.
    private final LorenzoTrack lorenzoTrack;
-   private final File lorenzoTrackConfigFile = new File("src/SquareConfig.json");
 
    public SinglePlayer(Controller controller) throws IOException {
       super(controller);
-      this.lorenzoTrack = GSON.lorenzoTrackParser(lorenzoTrackConfigFile);
+      this.lorenzoTrack = GSON.lorenzoTrackParser(ConfigParameters.lorenzoTrackConfigFile);
 
       this.actionTokenStack = new LinkedList<>(Arrays.asList(
               new DiscardToken(DevelopCardColor.BLUE), new DiscardToken(DevelopCardColor.GREEN),
