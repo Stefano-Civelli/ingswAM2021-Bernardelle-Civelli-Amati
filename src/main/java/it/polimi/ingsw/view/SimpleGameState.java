943b8891dc54491f35cf366b619c110414121c96
Pietro Bernardelle <pietro.bernardelle@mail.polimi.it>
2021-05-08 11:47:28 +0100

    implemented method constructMarket in SimpleGameState class. also modified some attribute type in the same class.

--- src/main/java/it/polimi/ingsw/view/SimpleGameState.java
+++ src/main/java/it/polimi/ingsw/view/SimpleGameState.java
@@ -1,24 +1,37 @@
 package it.polimi.ingsw.view;
 
-import it.polimi.ingsw.model.DevelopCard;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import it.polimi.ingsw.model.market.MarbleColor;
+import it.polimi.ingsw.network.messages.MessageType;
 import it.polimi.ingsw.utility.GSON;
 
 import java.util.List;
 
+
 public class SimpleGameState {
 
    private List<Integer>[][] developCardDeck; //identified by ID
-   private Integer[][] market;
+   private MarbleColor[][] market;
+   private MarbleColor slide;
 
 
    public void constructDeck(String jsonDeck) {
-      developCardDeck = GSON.getGsonBuilder().fromJson(jsonDeck, List[][].class);
-      for (List<Integer>[] x : developCardDeck)
-         for (List<Integer> y : x)
-            System.out.println(y);
+      this.developCardDeck = GSON.getGsonBuilder().fromJson(jsonDeck, List[][].class);
+//      for (List<Integer>[] x : developCardDeck)
+//         for (List<Integer> y : x)
+//            System.out.println(y);
       }
 
-      public void constructMarket () {
+      public void constructMarket (String jsonMarket) {
+         JsonObject jsonObject = (JsonObject) JsonParser.parseString(jsonMarket);
+         this.market = GSON.getGsonBuilder().fromJson(jsonObject.getAsJsonObject().get("key"), MarbleColor[][].class);
+         System.out.println("talla");
+         this.slide = GSON.getGsonBuilder().fromJson(jsonObject.getAsJsonObject().get("value").getAsString(), MarbleColor.class);
 
       }
+
+   public MarbleColor[][] getMarket() {
+      return market;
+   }
 }
