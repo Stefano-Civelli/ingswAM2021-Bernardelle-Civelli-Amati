ecf7b86d4d8f0c932c526521848264c3a4e09a32
Axelamati <alex.amati@mail.polimi.it>
2021-06-01 11:47:00 +0200

    Fixed bugs in GUI

--- src/main/java/it/polimi/ingsw/view/gui/controllers/GameboardController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/GameboardController.java
@@ -4,9 +4,7 @@ import it.polimi.ingsw.view.gui.SceneController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
