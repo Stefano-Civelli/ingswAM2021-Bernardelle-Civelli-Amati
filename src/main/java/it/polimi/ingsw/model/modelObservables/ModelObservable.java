4a347daa00c6d5dc6575ba3499f2e981b5a73062
Stefano Civelli <stefano.civelli@gmail.com>
2021-06-06 11:23:37 +0200

    samall fix in GUI

--- src/main/java/it/polimi/ingsw/model/modelObservables/ModelObservable.java
+++ src/main/java/it/polimi/ingsw/model/modelObservables/ModelObservable.java
@@ -6,9 +6,4 @@ import it.polimi.ingsw.model.ModelObserver;
 public interface ModelObservable {
 
   void notifyModelChange(String msg);
-
-  //void notifyReconnection(String username);
-
-  //void setController(ModelObserver controller);
-
 }
