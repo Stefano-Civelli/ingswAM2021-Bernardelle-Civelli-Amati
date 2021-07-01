539a4c23491504b5754ccd4219107244ec0eaf73
Stefano Civelli <stefano.civelli@gmail.com>
2021-07-01 15:19:34 +0200

    fixed previous commit

--- src/main/java/it/polimi/ingsw/view/cli/drawer/MarginConstructor.java
+++ src/main/java/it/polimi/ingsw/view/cli/drawer/MarginConstructor.java
@@ -13,7 +13,24 @@ public class MarginConstructor {
    */
   public static String[][] buildMargins(int row_dim, int col_dim){
     String[][] margins = new String[row_dim][col_dim];
+    int c, r;
 
+    margins[0][0] = "╔";
+    for (c = 1; c < col_dim - 1; c++)
+      margins[0][c] = "═";
+    margins[0][c] = "╗";
+
+    for (r = 1; r < row_dim - 1; r++) {
+      margins[r][0] = "║";
+      for (c = 1; c < col_dim - 1; c++)
+        margins[r][c] = " ";
+      margins[r][c] = "║";
+    }
+
+    margins[r][0] = "╚";
+    for (c = 1; c < col_dim - 1; c++)
+      margins[r][c] = "═";
+    margins[r][c] = "╝";
     return margins;
   }
 }
