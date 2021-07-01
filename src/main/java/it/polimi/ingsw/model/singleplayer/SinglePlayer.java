6d044b854034c7caf0e905a4b2c16b93e69c6a5e
Stefano Civelli <stefano.civelli@gmail.com>
2021-07-01 15:54:44 +0200

    fixed bug in Lorenzo track

--- src/main/java/it/polimi/ingsw/model/singleplayer/SinglePlayer.java
+++ src/main/java/it/polimi/ingsw/model/singleplayer/SinglePlayer.java
@@ -51,8 +51,12 @@ public class SinglePlayer extends Game {
     */
    @Override
    public void addPlayer(String username) throws IOException, MaximumNumberOfPlayersException {
-      if(super.getOrderedPlayers().size() == 0)
+      if(super.getOrderedPlayers().size() == 0) {
          super.addPlayer(username);
+         try {
+            this.lorenzoTrack.addToVaticanReportObserverList(getPlayerBoard(username).getTrack());
+         } catch (InvalidUsernameException e) { e.printStackTrace(); }
+      }
       else
          throw new MaximumNumberOfPlayersException(1);
    }
